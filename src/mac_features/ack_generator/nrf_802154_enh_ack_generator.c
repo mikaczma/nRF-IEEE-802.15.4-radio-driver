2c27a0772b4a8440a6974f4bd87f6a5359842784
Michal Kaczmarek <kaczmarekmichal.pl@gmail.com>
2019-11-28 02:08:32 +0100

    Removed blank comments

--- src/mac_features/ack_generator/nrf_802154_enh_ack_generator.c
+++ src/mac_features/ack_generator/nrf_802154_enh_ack_generator.c
@@ -355,7 +355,6 @@ const uint8_t * nrf_802154_enh_ack_generator_create(const uint8_t * p_frame)
         frame_offsets.src_addr_size == EXTENDED_ADDRESS_SIZE,
         &ie_data_len);
 
-    //
     if (p_ie_data != NULL)
     {
         uint8_t * p_ie_csl_header = (uint8_t *)nrf_802154_frame_parser_csl_ie_header_get(p_frame); // Const keyword removed by design to update data behind pointer
